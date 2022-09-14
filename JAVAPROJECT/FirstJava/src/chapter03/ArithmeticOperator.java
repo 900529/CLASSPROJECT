e92529c0b7db6a7fc7526c3d044ca66e5d67e307
900529 <yenaching529@gmail.com>
2022-09-14 17:08:17 +0900

    이항 연산자
    산술연산자

---
+++ JAVAPROJECT/FirstJava/src/chapter03/ArithmeticOperator.java
@@ -0,0 +1,24 @@
+package chapter03;
+
+public class ArithmeticOperator {
+
+	public static void main(String[] args) {
+		
+		int num1=15;
+		int num2=4;
+		float num3=2.0f;
+		
+		System.out.println(num1+num2);
+		System.out.println(num1-num2);
+		System.out.println(num1*num2);		
+		System.out.println(num1/num2);
+		System.out.println(num1/num3); //int+float -> float+float 으로 나오는데, 
+        System.out.println(num1%num2);
+        System.out.println(num1%num3);
+        
+        int result = num1 / 0;
+		
+        
+	}
+
+}
