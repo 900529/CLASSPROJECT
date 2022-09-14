5fb1e2ff958e4f1c95e432f92ec92a86261cbefa
900529 <yenaching529@gmail.com>
2022-09-14 16:19:49 +0900

    연산자 예제코드
    단항연산자

--- JAVAPROJECT/FirstJava/src/chapter02/Person.java
+++ JAVAPROJECT/FirstJava/src/chapter02/Person.java
@@ -8,7 +8,7 @@ public class Person {
           // ② 회원 전화번호 ( 000-0000-0000 )를 저장하는 변수
             String phoneNumber = null;
           // ③ 회원 주민등록번호 ( 000000-0000000 또는 0000000000000 )를 저장하는 변수
-            String personNumber = null;
+            String personId = null;
 	}
 
 }
