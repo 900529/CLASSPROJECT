9cc4a2d995c2c9c8eaff3fb40c47b833146996e1
900529 <yenaching529@gmail.com>
2022-09-14 17:06:11 +0900

    이항 연산자
    산술연산자 – 문자형 증감 연산

---
+++ JAVAPROJECT/FirstJava/src/chapter03/CharTypeOperator.java
@@ -0,0 +1,16 @@
+package chapter03;
+
+public class CharTypeOperator {
+
+	public static void main(String[] args) {
+		
+		char c1 = 'A';
+		int codeNum = c1 + 0;  //char+int → int + int = int
+		System.out.println(codeNum);
+		System.out.println(++c1); //66 출력하고 c1 = 66
+		System.out.println(c1++); //66 먼저 가지고 있는거 출력하고 증가 c1 = 67
+		System.out.println(++c1); //증가부터 시키고 c1 = 68
+		
+	}
+
+}
