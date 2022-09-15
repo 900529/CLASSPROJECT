29175f9d278229ece6d029a20b4d3ab2ff56fe8b
900529 <yenaching529@gmail.com>
2022-09-15 10:52:21 +0900

    관계연산자
    참조형 변수의 비교

--- JAVAPROJECT/FirstJava/src/chapter03/RelationalOperator.java
+++ JAVAPROJECT/FirstJava/src/chapter03/RelationalOperator.java
@@ -7,20 +7,20 @@ public class RelationalOperator {
 		//관계연산자 (비교연산자)
 		// >, <, >=, <=, ==, !=
 		
-		int num1=30;
-	    int num2 =2;
-	    
-	    System.out.println(num1>num2);   //true
-	    System.out.println(num1<num2); //false
-	    System.out.println(num1>=num2); //true
-	    System.out.println(num1<=num2); //false
-	    System.out.println(num1 ==num2); //false
-	    System.out.println(num1!=num2); //trues 
-	    
-	    if(num1>num2) {
-	    	//참일 때 처리되는 블록
-	    	System.out.println("num1이 num2보다 큽니다.");
-	    	
+		//참조변수 : 주소를 가지는 변수
+        //주소 : 객체를 가르키는 주소 값 
+		// 크다 작다 비교는 하지 않는다! 
+		// 같은 주소를 가지는지 확인하는 연산이 주로 사용된다. 
+		
+		String str1 ="JAVA";  //str1에는 실제적으로 객체를 가르키는 주소가 들어감 
+		String str2 = "HTML";  
+		String str3 = "JAVA";
+				
+		System.out.println(str1 == str2); // 주소값 == 주소값 
+		System.out.println(str1 != str2); //주소값 !=주소값
+		System.out.println("str1 == str3 =>"+ (str1 == str3));  // ("str1 == str3 =>"+ str1 == str3); 이렇게 할 경우 + 때문에 오류가 나옴 
+		
+		// " " 하게되면 객체 만드는데 
 	    	
 	    }
 	    
@@ -28,4 +28,4 @@ public class RelationalOperator {
 
 	}
 
-}
+
