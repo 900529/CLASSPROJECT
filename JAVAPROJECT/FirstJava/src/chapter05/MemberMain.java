4b0c4da562d03f9b0f1e7b62d5d872762968234f
900529 <yenaching529@gmail.com>
2022-09-20 00:08:54 +0900

    들여쓰기

--- JAVAPROJECT/FirstJava/src/chapter05/MemberMain.java
+++ JAVAPROJECT/FirstJava/src/chapter05/MemberMain.java
@@ -2,16 +2,16 @@ package chapter05;
 
 public class MemberMain {
 
-	public static void main(String[] args) { 
-	
-	Member member1 = new Member ("최예나", "010-0000-0000", "역사", 4, "yena@daum.net", "5월29일", "서울시 송파구");
-	member1.printMemberData();
-	
-	System.out.println("==================");
-	
-	Member member2 = new Member ("이바름", "010-0000-0000", "영어", 3, "br@naver.com");
-	member2.printMemberData();
-	
+	public static void main(String[] args) {
+
+		Member member1 = new Member("최예나", "010-0000-0000", "역사", 4, "yena@daum.net", "5월29일", "서울시 송파구");
+		member1.printMemberData();
+
+		System.out.println("==================");
+
+		Member member2 = new Member("이바름", "010-0000-0000", "영어", 3, "br@naver.com");
+		member2.printMemberData();
+
 	}
 
 }
