5feade25ace311f03dcedb9a69f09234eb4feb9d
900529 <yenaching529@gmail.com>
2022-09-20 20:55:22 +0900

    과제

--- OOPMiniProject/src/ver01/ContactMain.java
+++ OOPMiniProject/src/ver01/ContactMain.java
@@ -6,7 +6,7 @@ public class ContactMain {
 
 	public static void main(String[] args) {
 
-		Contact con = new Contact("최예나", "010-0000-0000", "yn@naver.com", "송파구", "5월1일");
+		Contact con = new Contact("예나", "010-0000-0000", "yn@naver.com", "송파구", "5월1일");
 
 		System.out.println("이름 : " + con.getName());
 		System.out.println("번호 : " + con.getPhoneNumber());
