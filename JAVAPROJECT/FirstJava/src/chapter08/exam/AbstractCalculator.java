0dda9a2e67a1bdfc30466fc0e099ccd6035f0eca
900529 <yenaching529@gmail.com>
2022-09-27 23:07:56 +0900

    예제

--- JAVAPROJECT/FirstJava/src/chapter08/exam/AbstractCalculator.java
+++ JAVAPROJECT/FirstJava/src/chapter08/exam/AbstractCalculator.java
@@ -1,9 +1,9 @@
 package chapter08.exam;
 
+// 1. Calulator 인터페이스를 상속하는 추상 클래스 정의
 public abstract class AbstractCalculator implements Calculator {
 
-	// 1. Calulator 인터페이스를 상속하는 추상 클래스를 정의해봅시다.
+	// 내부에서 쓰는 변수
 	private double pi = 3.14;
-	
-	
+
 }
