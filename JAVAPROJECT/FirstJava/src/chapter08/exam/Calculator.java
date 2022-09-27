6194c2ba2edbad1e447bb1556a50afb368ca75e3
900529 <yenaching529@gmail.com>
2022-09-27 12:27:01 +0900

    수업

---
+++ JAVAPROJECT/FirstJava/src/chapter08/exam/Calculator.java
@@ -0,0 +1,10 @@
+package chapter08.exam;
+// 인터페이스로 생성 
+public interface Calculator { 
+
+	long add(long n1, long n2);  // public abstract 생략 
+	long substract (long n1, long n2); 
+	long multiply (long n1, long n2);
+	double divide(double n1, double n2);
+	
+}
